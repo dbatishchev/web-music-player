43b937d8b77ac891d03b4cf1b7622115790e937f
dimitrybatishchev@gmail.com <dimitrybatishchev@gmail.com>
2015-04-03 10:13:39 +0300

    add search page

--- front/js/pages/search/search.js
+++ front/js/pages/search/search.js
@@ -21,5 +21,19 @@ angular.module('Music.search', [
     .controller('SearchCtrl', function($scope, $location, $sce, $routeParams, vk, player) {
         console.log($routeParams.q);
 
+        $.getJSON("http://ws.audioscrobbler.com/2.0/?method=album.search&album="+$routeParams.q+"&api_key=ff756adbaf5b90f9d733fa0fdaae91af&format=json", function(data){
+            $scope.$apply(function () {
+                $scope.albums = data.results.albummatches.album;
+                console.log($scope.albums);
+            });
+        })
+
+        $scope.getSanitazedString = function(string){
+            return string.replace(/ /g, '+')
+        }
+
+        $scope.getAlbumUrl = function(album){
+            return album.artist.name.replace(/ /g, '+') + '/' + album.name.replace(/ /g, '+');
+        }
     })
 });
