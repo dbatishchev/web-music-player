cbb1f0f1ed8fb2b8d3ee8b6ed6d9598a4365d26e
dimitrybatishchev@gmail.com <dimitrybatishchev@gmail.com>
2015-04-03 15:04:59 +0300

    add sqlite

--- web_music_player/settings.py
+++ web_music_player/settings.py
@@ -60,12 +60,12 @@ WSGI_APPLICATION = 'web_music_player.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'music',
-        'USER': 'root',
-        'PASSWORD': 'Igrima123',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':  os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'USER': 'root',
+        #'PASSWORD': 'Igrima123',
+        #'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        #'PORT': '3306',
     }
 }
 
